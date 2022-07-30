3190a92d7c607e930368c8a032c6ac0c0af2bbab
Kooper-kosh <katsusara11@gmail.com>
2022-07-30 21:38:52 +0800

    Updates

---
+++ precache-manifest.eb18b114db73e328fc9714b4ccd1d70d.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "880da30ed74251e9c7aa",
+    "url": "/portfolio-kat/static/css/main.a43abed4.chunk.css"
+  },
+  {
+    "revision": "880da30ed74251e9c7aa",
+    "url": "/portfolio-kat/static/js/main.880da30e.chunk.js"
+  },
+  {
+    "revision": "72f5baf153d2f53e2870",
+    "url": "/portfolio-kat/static/css/1.73c455ea.chunk.css"
+  },
+  {
+    "revision": "72f5baf153d2f53e2870",
+    "url": "/portfolio-kat/static/js/1.72f5baf1.chunk.js"
+  },
+  {
+    "revision": "ac0891b98a14a732273b",
+    "url": "/portfolio-kat/static/js/runtime~main.ac0891b9.js"
+  },
+  {
+    "revision": "42e466f010730f5ec82fbc217bd83530",
+    "url": "/portfolio-kat/static/media/whitebg.42e466f0.JPG"
+  },
+  {
+    "revision": "d088d93cf0b893d29baf0993ef52c663",
+    "url": "/portfolio-kat/static/media/logo.d088d93c.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/portfolio-kat/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/portfolio-kat/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/portfolio-kat/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/portfolio-kat/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "70d600e220f3d440e03482ff785cb5fd",
+    "url": "/portfolio-kat/static/media/EXCELLENCE IN POSTER - CHEKKA.70d600e2.png"
+  },
+  {
+    "revision": "55d50758115145533631c29ff36b8f71",
+    "url": "/portfolio-kat/static/media/PLV_BackEndDevelopmentUsingMVCFramework.55d50758.JPG"
+  },
+  {
+    "revision": "92ab452e1a634c33c2a817bca1dece70",
+    "url": "/portfolio-kat/static/media/PLV_CreativeDesign2DGraphicsWithPhotoshopUsingShapes.92ab452e.jpg"
+  },
+  {
+    "revision": "3431b7d3f64fce236f0089d62276d923",
+    "url": "/portfolio-kat/static/media/PLV_CybersecurityGoodnessInHacking.3431b7d3.jpg"
+  },
+  {
+    "revision": "e58f35b28c80948c278ddea17cda3d6c",
+    "url": "/portfolio-kat/static/media/PLV_DICTBuildingICTResilientNationInAPhygitalWorld.e58f35b2.JPG"
+  },
+  {
+    "revision": "10ddbf3df01bfc4aa5114a7c3c810941",
+    "url": "/portfolio-kat/static/media/PLV_E-commerceTheRoleOfModernTechnologyInBusinessAndEntrepreneurs.10ddbf3d.JPG"
+  },
+  {
+    "revision": "56e9e0dc18626e422e81b2eeab5906ff",
+    "url": "/portfolio-kat/static/media/PLV_HowToBecomeAHybridDeveloper.56e9e0dc.PNG"
+  },
+  {
+    "revision": "d5d67f098a83c77872d5af5d84f5c5bd",
+    "url": "/portfolio-kat/static/media/PLV_HowToGetAnEntryLevelOfITJob.d5d67f09.jpg"
+  },
+  {
+    "revision": "23ffd05c90007247f7ec7bf340d8d3c0",
+    "url": "/portfolio-kat/static/media/PLV_IntroductionToDataAnalyticsAndDataScience.23ffd05c.jpg"
+  },
+  {
+    "revision": "6c5e86f7bf50f8ff6bcd07c92e348fe5",
+    "url": "/portfolio-kat/static/media/PLV_IntroductionToHumanCenteredDesign.6c5e86f7.jpg"
+  },
+  {
+    "revision": "de1d547b75f8c972083244dd90005887",
+    "url": "/portfolio-kat/static/media/PLV_MachineLearningNaiveBayesAndKNNClassofocationModel.de1d547b.JPG"
+  },
+  {
+    "revision": "7b6dbd392a7889b24bc16e05c45594ef",
+    "url": "/portfolio-kat/static/media/PLV_ProjectManagementAndVersionControlUsingGithub.7b6dbd39.JPG"
+  },
+  {
+    "revision": "fdcfc20e2eb8d8829a0136a9adfb6078",
+    "url": "/portfolio-kat/static/media/PLV_The2022RoadmapToBecomingAFullStackJavaScriptDeveloper.fdcfc20e.JPG"
+  },
+  {
+    "revision": "be853dda83d42da240dcb844651f70e8",
+    "url": "/portfolio-kat/static/media/PLV_UsingMobilePhonesForFilmMakingAndPhotography.be853dda.JPG"
+  },
+  {
+    "revision": "e87d6c16169dab42a8846c10bc241582",
+    "url": "/portfolio-kat/static/media/PLV_VFXPipelineAndCGIInThePhilippines.e87d6c16.JPG"
+  },
+  {
+    "revision": "d596cfc7047d9c6d164ffd4514487c39",
+    "url": "/portfolio-kat/static/media/PLV_WebApplicationUsingGoogleFirebaseRealTimeDatabase.d596cfc7.JPG"
+  },
+  {
+    "revision": "85a4ddcb28db8f87acca5d7482f6fe9e",
+    "url": "/portfolio-kat/static/media/PLV_WorkingSmartAsAnITDeveloper.85a4ddcb.png"
+  },
+  {
+    "revision": "19b4ec3670f5b8b546f53fb59e1a9988",
+    "url": "/portfolio-kat/static/media/PLV_IntroductionToAgile.19b4ec36.jpg"
+  },
+  {
+    "revision": "1c9bcc9d11ad4ccf2c80e16fad42efb4",
+    "url": "/portfolio-kat/static/media/PLV_InternetOfThings.1c9bcc9d.jpg"
+  },
+  {
+    "revision": "96fbee56060aa4e075adf61e049ebad2",
+    "url": "/portfolio-kat/static/media/Out_factchecking.96fbee56.JPG"
+  },
+  {
+    "revision": "d4e8fe59f0bb065badce8de46a6b5bbd",
+    "url": "/portfolio-kat/static/media/Out_FundamentalsOfCloud.d4e8fe59.JPG"
+  },
+  {
+    "revision": "cf5a25f8dd0292d55fc5ab8beecfe1ac",
+    "url": "/portfolio-kat/static/media/Out_HiGen-Filmora.cf5a25f8.jpg"
+  },
+  {
+    "revision": "3a4acc72c300f5b3d815d82b03d4af87",
+    "url": "/portfolio-kat/static/media/Out_machinelearning.3a4acc72.JPG"
+  },
+  {
+    "revision": "472709891acf3184c224964d4624cdbe",
+    "url": "/portfolio-kat/static/media/Out_progressive.47270989.JPG"
+  },
+  {
+    "revision": "705ace109f251de3de1aa32a6bddea00",
+    "url": "/portfolio-kat/static/media/Out_QCU-Cryptography.705ace10.png"
+  },
+  {
+    "revision": "17a2e307e1daa1b98bd503b60415e8b0",
+    "url": "/portfolio-kat/static/media/Out_UPSKILL-NLP.17a2e307.JPG"
+  },
+  {
+    "revision": "87bc4c1ae605b2f0c4de0dfa7bbe56ad",
+    "url": "/portfolio-kat/static/media/Out_wordpress.87bc4c1a.JPG"
+  },
+  {
+    "revision": "6468634d7642b247f4a6247b94d05553",
+    "url": "/portfolio-kat/static/media/left-arrow.6468634d.svg"
+  },
+  {
+    "revision": "279196e37844ff702af163035a67c609",
+    "url": "/portfolio-kat/static/media/right-arrow.279196e3.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio-kat/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/portfolio-kat/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/portfolio-kat/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/portfolio-kat/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "3f2f75c66e05a4e0dd3e986254e5f4c9",
+    "url": "/portfolio-kat/static/media/bg-3.3f2f75c6.jpg"
+  },
+  {
+    "revision": "7122371d6f7519e714d1b8235defaa06",
+    "url": "/portfolio-kat/static/media/katdp-transformed.7122371d.png"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/portfolio-kat/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolio-kat/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolio-kat/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portfolio-kat/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/portfolio-kat/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/portfolio-kat/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "5841e8a4ea4682d2f31359d467f00db6",
+    "url": "/portfolio-kat/static/media/PLV_IntroductionToProgressiveWebApps.5841e8a4.JPG"
+  },
+  {
+    "revision": "758c1e3099fe0b935cc4520bf8090736",
+    "url": "/portfolio-kat/index.html"
+  }
+];
